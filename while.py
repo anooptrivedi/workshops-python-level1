84ef10e63b3796f5e0bd45bd9c1061ade4ed92ac
anooptrivedi <anoop4@gmail.com>
2014-03-24 21:28:47 -0700

    example while statement

---
+++ while.py
@@ -0,0 +1,7 @@
+while(True):
+    count = int(input('Please enter a number: '))
+    if (count >= 5):
+        print(count, ' is more than or equal to 5')
+    else:
+        print(count, ' is less than 5')
+        exit()
